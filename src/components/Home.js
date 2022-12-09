4874954bdbcc025aa107b85fda39f76b82f4516e
ralf-t <tabo.ralfmartin@ue.edu.ph>
2022-12-09 14:43:42 +0800

    sell and add coins based on price

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,10 @@ const Home = () => {
     // Constants
     const TIME = 2000; // update robot every 2 seconds
     const PRICE_RATE = 0.05; // 5% growth in price
+
+    // total coins
+    const [coins, setCoins] = useState(0)
+
     //List of owned robots
     const [robots, setRobots] = useState([
         {
@@ -124,6 +128,13 @@ const Home = () => {
     const [isOpen, setIsOpen] = useState(false)
     //Sell robot 
     const sellRobot = (id) => {
+        
+        robots.find(robot => {
+            if (robot.id == id) {
+                setCoins(coins + robot.price)
+            }
+        })
+
         setRobots(robots.filter((robot) => robot.id !== id))
         const oldRobot = robots.find(x => x.id === id)
         setShopRobots([...shopRobots,oldRobot])
@@ -165,7 +176,7 @@ const Home = () => {
 
     return (
         <div className='home'>    
-            <HomeHeader setIsOpen={setIsOpen}/>
+            <HomeHeader setIsOpen={setIsOpen} coins={coins}/>
             <div className='home-container'>
                 { robots.length > 0 ?
                 <div className='robot-container'>
--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -2,14 +2,14 @@ import logo from '../assets/logo.png'
 import coinBar from '../assets/coin-bar.png'
 import avatar from '../assets/avatar.png'
 
-const HomeHeader = ({ setIsOpen }) => {
+const HomeHeader = ({ setIsOpen, coins }) => {
     return (
         <div className='header'>
             <img src={logo} className='logo' alt='Cryptopunkz Logo' />
             <div className='header-info'>
                 <div className='coin-bar'>
                     <img src={coinBar} alt='Coins' />
-                    <span>999999</span>
+                    <span>{coins}</span>
                 </div>
                 <input type="button" className='shop-btn' onClick={() => setIsOpen(true)}/>
                 <img src={avatar} className='avatar' alt='Cryptopunkz Logo' />
