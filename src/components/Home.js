2d6336d7573f219debc4dfff745650e7cbeb79df
Iionsroar <yawned.eyes@gmail.com>
2022-12-09 11:47:10 +0800

    add info defaults

--- src/components/Home.js
+++ src/components/Home.js
@@ -57,16 +57,7 @@ const Home = () => {
         },
     ])
     //selected robot
-    const [robotInfo, setRobotInfo] = useState(
-        {
-            id: 1,
-            robotName: "Robot 1",
-            image: require('../assets/rob1.gif'),
-            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-            currentLevel: 1,
-            price: 100
-        }
-    )
+    const [robotInfo, setRobotInfo] = useState(robots[0])
     //shop robot
     const [shopRobots, setShopRobots]=useState([
         {
@@ -124,6 +115,19 @@ const Home = () => {
         setRobots(robots.filter((robot) => robot.id !== id))
         const oldRobot = robots.find(x => x.id === id)
         setShopRobots([...shopRobots,oldRobot])
+        if (robots.length > 1) {
+            // set to index 1 instead of 0 (first item) because for some reason, the robots array does not get updated immediately upon selling
+            setRobotInfo(robots[0]) 
+        } else {
+            setRobotInfo({
+                    id: null,
+                    robotName: null,
+                    image: null,
+                    description: "No robots at home. Quick! Purchase one!",
+                    currentLevel: 0,
+                    price: 0
+                })
+        }
     }
     //Set selected robot
     const selectRobot = (id) => {
@@ -135,6 +139,7 @@ const Home = () => {
         setShopRobots(shopRobots.filter((shopRobot) => shopRobot.id !== id ))
         const newRobot = shopRobots.find(x => x.id === id)
         setRobots([...robots,newRobot])
+        setRobotInfo(newRobot)
         console.log("hello", id)
     }
     return (
